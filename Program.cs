f3a5baeb1003cc2628384031ecda671fabd6944c
santhoshdonthi <36616264+santhoshdonthi@users.noreply.github.com>
2025-07-23 07:53:28 +0530

    check

--- Program.cs
+++ Program.cs
@@ -1,13 +1,51 @@
 using APIAuthentication.Middleware;
 using APIAuthentication.Services;
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<TokenService>();
-// Add services to the container.
 
+// Add services to the container.
+//builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API Authentication",
+        Version = "v1",
+        Description = "OAuth2 Authentication Gateway with Encryption Middleware"
+    });
+
+    // Add support for Bearer token authentication
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your token in the text input below.\nExample: 'Bearer abc123xyz'"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+builder.Services.AddSingleton<TokenService>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 app.UseMiddleware<EncryptionMiddleware>(true);
